d1df00e02b9134cffa995af1ba1f7387ac3582fe
Sachin S <shindesachin@hotmail.com>
2021-02-02 17:04:42 +0530

    verify the data passed through request is posted correctly

--- src/test/java/JiraTest.java
+++ src/test/java/JiraTest.java
@@ -1,5 +1,7 @@
 import io.restassured.RestAssured;
 import io.restassured.filter.session.SessionFilter;
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 
 import java.io.File;
 
@@ -24,26 +26,58 @@ public class JiraTest {
 
 
         //add comment
-        given().pathParam("key","10001").
+
+        String expectedMessage = "Expected API message" ;
+
+        String addComment = given().pathParam("key","10001").
                 header("Content-Type","application/json").
                 filter(sessionFilter).
                 body("{\n" +
-                        "    \"body\": \"New Comment.\",\n" +
+                        "    \"body\": \""+expectedMessage+"\",\n" +
                         "    \"visibility\": {\n" +
                         "        \"type\": \"role\",\n" +
                         "        \"value\": \"Administrators\"\n" +
                         "    }\n" +
                         "}").
                 log().all().when().
-                post("/rest/api/2/issue/{key}/comment").then().assertThat().statusCode(201);
+                post("/rest/api/2/issue/{key}/comment").then().assertThat().statusCode(201).
+                extract().response().asString();
+
+        //get comment id
+
+        JsonPath js = new JsonPath(addComment);
+        String id = js.getString("id");
 
         // Add attachment
         given().header("X-Atlassian-Token", "no-check").filter(sessionFilter).
+                pathParam("key","10001").
                 header("Content-Type","multipart/form-data").
                 multiPart("file", new File("Jira.txt")).when().
-                post("rest/api/2/issue/10001/attachments").then().log().all().
+                post("rest/api/2/issue/{key}/attachments").then().log().all().
                 assertThat().statusCode(200);
 
 
-    }
+        //Get Issue
+
+        String getResponse = given().filter(sessionFilter).pathParam("key", "10001").log().all().
+                when().get("rest/api/2/issue/{key}").then().assertThat().statusCode(200).
+                extract().response().asString();
+        System.out.println(getResponse);
+
+        // verify the comment passed through request is posted correctly
+
+        JsonPath js1 = new JsonPath(getResponse) ;
+        int commentsNumber = js1.getInt("fields.comment.comments.size()") ;
+
+        for (int i=0; i<commentsNumber; ++i) {
+
+            if(js1.getString("fields.comment.comments["+i+"].id").toString().equalsIgnoreCase(id)) {
+                String actualMessage = js1.getString("fields.comment.comments["+i+"].body").toString();
+                Assert.assertEquals(actualMessage ,expectedMessage);
+
+            }
+
+        }
+}
+
 }
